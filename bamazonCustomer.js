d42d068a12e6ea6239cd3ae42e7877ad8b2cfc64
clmitchell630 <clmitchell630@gmail.com>
2019-09-30 20:34:30 -0500

    bamazon... got in a groove but there wasn't much to the parts of it

---
+++ bamazonCustomer.js
@@ -0,0 +1,83 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "3Dpotatocode",
+    database: "bamazon_db"
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+
+    connection.query("SELECT * FROM products", (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        purchasePrompt();
+    });
+
+});
+
+
+function purchasePrompt() {
+    inquirer.prompt([
+        {
+            name: "id",
+            type: "input",
+            message: "What is the id of the item you would like to buy?",
+            validate: (value) => {
+                return !isNaN(value);
+            }
+        }, {
+            name: "units",
+            type: "input",
+            message: "How many units of this product would you like to buy?",
+            validate: (value) => {
+                return !isNaN(value);
+            }
+        }
+    ]).then((answer) => {
+
+        connection.query(
+            `SELECT * FROM products WHERE item_id=${answer.id}`,
+            (err, res) => {
+                if (err) throw err;
+
+                console.table(res);
+
+                if (answer.units < res[0].stock_quantity) {
+
+                    var maths = res[0].stock_quantity - answer.units;
+                    var priceMaths = res[0].price * answer.units;
+                    connection.query(
+                        `UPDATE products SET ? WHERE ?`,
+                        [
+                            {
+                                stock_quantity: maths
+                            }, {
+                                item_id: answer.id
+                            }
+                        ],
+                        (err) => {
+                            if (err) throw err;
+
+                            console.log(`Purchasing ${answer.units} units of ${res[0].product_name} for $${priceMaths}! Thank you for your purchase!`);
+
+                        }
+                    );
+                } else {
+                    connection.end();
+                    return console.log(`Insufficient quantity!`);
+                    
+                }
+
+                connection.end();
+
+            }
+        );
+
+    });
+}
---
+++ schema.sql
@@ -0,0 +1,29 @@
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
+
+USE bamazon_db;
+
+CREATE TABLE products(
+	item_id INTEGER(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(100) NULL,
+    department_name VARCHAR(100) NULL,
+    price DECIMAL(10,2) NULL,
+    stock_quantity INTEGER(10) NULL
+);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("Anti Snoring Smart Pillow", "Bedding", 378.00, 7),
+("Men Leather Leisure Wallet", "Mens Apparel", 7.19, 20),
+("Kradl Bike Mount", "Sporting Goods", 169.00, 11),
+("PEN's Battle II Fishing Rod", "Sporting Goods", 119.99, 8),
+("Have You Seen Luis Valez?", "Books", 7.99, 103),
+("Cactus Bath Rug", "Home Goods", 3.45, 18),
+("Classy Potato Gift Bundle", "Grocery", 21.99, 3),
+("Gusleson Classic Wedding Tie", "Mens Apparel", 5.90, 87),
+("Griot's Garage Random ORbital Polisher", "Automotive", 149.99, 15),
+("Yothg 580mL Leakproof Waterbottle", "Sporting Goods", 9.99, 21);
+
+SELECT * FROM products;
+
+
+    
